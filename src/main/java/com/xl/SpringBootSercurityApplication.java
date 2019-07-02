46907d4908e96e9d5e300740b1fbfb58b127bb78
xiongliang <137010393@qq.com>
2019-07-02 18:39:28 +0800

    sercurity

---
+++ src/main/java/com/xl/SpringBootSercurityApplication.java
@@ -0,0 +1,13 @@
+package com.xl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSercurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSercurityApplication.class, args);
+    }
+
+}
